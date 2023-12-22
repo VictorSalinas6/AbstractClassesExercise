f1a6b614c38c84bc5eda360f7f68c527cbc6ad4b
Victor Salinas <victorsalinas6@gmail.com>
2023-12-22 10:28:57 -0600

    Did the abstract class Vehicle

---
+++ ConsoleUI/Vehicle.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI
+{
+    public abstract class Vehicle
+    {
+        public string Year { get; set; } = "19XX";
+        public string Make { get; set; } = "XXXX";
+        public string Model { get; set; } = "XXXX";
+
+        public abstract void DriveAbstract();
+        public virtual void DriveVirtual()
+        {
+
+        }
+
+    }
+}
