eb78e41849e00cb86fd84660c10811c80d5ea0f3
Victor Salinas <victorsalinas6@gmail.com>
2023-12-22 10:40:36 -0600

    Created the 2 non abstract classes

---
+++ ConsoleUI/Motorcycle.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI
+{
+    internal class Motorcycle : Vehicle
+    {
+        public bool HasSideCart { get; set; }
+        public int Passengers { get; set; }
+
+        public override void DriveAbstract()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void DriveVirtual()
+        {
+            base.DriveVirtual();
+        }
+    }
+}
